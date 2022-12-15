6b2d3f9bb1cdb5a453658c0de5e35befe9edf283
alok srivastava <alokaryan@gmail.com>
2022-12-15 15:55:15 +0530

    instance type changed to t2.large

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_instance" "ourfirst" {
   ami           = "ami-0447a12f28fddb066"
-  instance_type = "t2.micro"
+  instance_type = "t2.large"
   tags = {
     Name = "sentinel-server"
   }
