8015b2a4b80837413755357ab413ef43f15623ac
alokaryan <alokaryan@gmail.com>
2023-07-30 15:58:04 +0530

    updated

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ provider "aws" {
 resource "aws_instance" "ourfirst" {
   ami           = "ami-0447a12f28fddb066"
   instance_type = "t2.small"
-  tags = {
-    Name = "sentinel-server"
-  }
+#  tags = {
+#    Name = "sentinel-server"
+#  }
   
 }
